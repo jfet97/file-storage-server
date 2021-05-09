613b273d4f28bff16984efad4128dfccb465e7d4
Andrea Simone Costa <jfet97@users.noreply.github.com>
2021-05-09 23:31:38 +0200

    fix bug and extend thread safety (#2)

--- simple-queue/simple_queue.h
+++ simple-queue/simple_queue.h
@@ -9,35 +9,17 @@
 #define E_SQ_GENERAL 3
 #define E_SQ_EMPTY_QUEUE 4
 #define E_SQ_MUTEX_COND 5
+#define E_SQ_QUEUE_DELETED 6
+#define E_SQ_NO_QUEUE 7
+#define E_SQ_NULL_ELEMENT 8
 
-typedef struct SimpleNode
-{
-    void *element;
-    struct SimpleNode *next;
-    struct SimpleNode *prev;
-} SimpleNode;
-
-typedef struct SimpleListEntity
-{
-    SimpleNode *head;
-    SimpleNode *tail;
-    size_t length;
-} SimpleListEntity;
-
-typedef SimpleListEntity *List;
-
-typedef struct SimpleQueue
-{
-    List queue;
-    pthread_mutex_t lock;
-    pthread_cond_t read_cond;
-} SimpleQueue;
+typedef struct SimpleQueue *SimpleQueue;
 
 // queue
-SimpleQueue *SimpleQueue_create(int *error);
-void SimpleQueue_delete(SimpleQueue **queuePtr, int *error);
-void SimpleQueue_enqueue(SimpleQueue *queue, void *element, int *error);
-void *SimpleQueue_dequeue(SimpleQueue *queue, int wait, int *error);
-size_t SimpleQueue_length(SimpleQueue *queue, int *error);
+SimpleQueue SimpleQueue_create(int *error);
+void SimpleQueue_delete(SimpleQueue *queuePtr, int *error);
+void SimpleQueue_enqueue(SimpleQueue queue, void *element, int *error);
+void *SimpleQueue_dequeue(SimpleQueue queue, int wait, int *error);
+size_t SimpleQueue_length(SimpleQueue queue, int *error);
 const char *SimpleQueue_getErrorMessage(int errorCode);
 #endif
