493d5dcde8d2710007a6ea3708fd203500f3ccd3
Andrea Simone Costa <andrysimo1997@gmail.com>
2021-05-13 00:24:49 +0200

    implement simple vesion of a command line parser

---
+++ command-line-parser/command-line-parser.c
@@ -0,0 +1,271 @@
+#include "command-line-parser.h"
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#define WAIT_OP "WAIT_OP"
+#define WAIT_PARAM "WAIT_PARAM"
+
+
+struct Arguments
+{
+    Option *internal_list;
+};
+
+Option *Option_create(char op, char *param)
+{
+    Option *newOption = malloc(sizeof(*newOption));
+
+    if (newOption)
+    {
+        newOption->op = op;
+        newOption->param = param;
+    }
+
+    return newOption;
+}
+
+void Option_free(Option **optionPtr)
+{
+    free(*optionPtr);
+    *optionPtr = NULL;
+}
+
+int insert(Option **listPtr, Option *newNode)
+{
+    int codeToRet = 0;
+
+    if (listPtr && newNode)
+    {
+        newNode->next = *listPtr;
+        *listPtr = newNode;
+    }
+    else
+    {
+        codeToRet = -1;
+    }
+
+    return codeToRet;
+}
+
+void forceFree(Option **listPtr)
+{
+    if (listPtr && *listPtr)
+    {
+        forceFree(&(*listPtr)->next);
+        Option_free(listPtr);
+    }
+}
+
+int isOpCountDashes(const char *str)
+{
+    int dashCounter = 0;
+
+    if (str != NULL)
+    {
+        // leggo e conto i dash
+        while (*str++ == '-')
+        {
+            dashCounter++;
+        }
+    }
+    else
+    {
+        dashCounter = -1;
+    }
+
+    // if(dashCounter == 0) e' stato letto un param
+
+    return dashCounter;
+}
+
+Arguments CommandLineParser_parseArguments(char *argv[], int *error)
+{
+    int errToSet = 0;
+    const char *arg = NULL;
+    const char *state = WAIT_OP;
+    Arguments as = NULL;
+    Option *option = NULL;
+
+    if (argv == NULL)
+    {
+        errToSet = E_CLP_WRONG_INPUT;
+    }
+
+    if (!errToSet)
+    {
+        as = malloc(sizeof(*as));
+
+        if (as == NULL)
+        {
+            errToSet = E_CLP_MALLOC;
+        }
+
+        as->internal_list = NULL;
+    }
+
+    // per ogni argomento
+    while (!errToSet && (arg = *argv++) != NULL)
+    {
+
+        // determino lo stato corrente
+        int is_WAIT_OP = strncmp(WAIT_OP, state, strlen(WAIT_OP)) == 0;
+        int is_WAIT_PARAM = strncmp(WAIT_PARAM, state, strlen(WAIT_PARAM)) == 0;
+
+        // conto il numero di dash per determinare cosa ho letto: op o param?
+        int dashCounter = isOpCountDashes(arg);
+
+        if (dashCounter == 0 && is_WAIT_OP)
+        {
+            // e' stato letto un param solitario da ignorare
+            continue;
+        }
+
+        if (dashCounter == 0 && is_WAIT_PARAM)
+        {
+            // e' stato letto il param che attendevo
+            option->param = arg;
+            insert(&(as->internal_list), option);
+            option = NULL;
+
+            // successivamente aspetto una nuova op
+            state = WAIT_OP;
+            continue;
+        }
+
+        if (dashCounter != 0 && is_WAIT_PARAM)
+        {
+            // e' stata letta una nuova op quando attendevamo il param della precedente
+
+            // param che allora non esiste
+            option->param = NULL;
+            insert(&(as->internal_list), option);
+            option = NULL;
+        }
+
+        // ho letto una nuova op che trovo in arg+dashCounter
+        arg += dashCounter;
+
+        // puts(arg);
+
+        // prelevo l'option
+        const char op = *arg++;
+
+        // printf("op %c\n", op);
+
+        // creo la nuova option
+        option = Option_create(op, NULL);
+
+        if (!option)
+        {
+            errToSet = E_CLP_MALLOC;
+            continue;
+        }
+
+        if (*arg == '\0')
+        {
+            // ho letto un opzione senza parametro che devo cercare di leggere nel prossimo argomento
+            state = WAIT_PARAM;
+            continue;
+        }
+        else
+        {
+            // prendo come parametro il rimanente dell'argomento
+            option->param = arg;
+            insert(&(as->internal_list), option);
+            option = NULL;
+
+            // attendo prossima op
+            state = WAIT_OP;
+            continue;
+        }
+    }
+
+    if (!errToSet && option != NULL && strncmp(WAIT_PARAM, state, strlen(WAIT_PARAM)) == 0)
+    {
+        // è rimasta in sospeso una option senza parametro -> param sarà NULL
+        option->param = NULL;
+        insert(&(as->internal_list), option);
+    }
+
+    if (errToSet)
+    {
+        as ? forceFree(&(as->internal_list)) : NULL;
+        as ? free(as) : NULL;
+        option ? Option_free(&option) : NULL;
+        as = NULL;
+    }
+
+    error && (*error = errToSet);
+    return as;
+}
+
+void CommandLineParser_delete(Arguments *argsPtr, int *error)
+{
+    int errToSet = 0;
+
+    if (argsPtr == NULL || *argsPtr == NULL)
+    {
+        errToSet = E_CLP_NULL;
+    }
+    else
+    {
+        forceFree(&((*argsPtr)->internal_list));
+        free((*argsPtr));
+        *argsPtr = NULL;
+    }
+
+    error && (*error = errToSet);
+}
+
+void CommandLineParser_printArguments(Arguments args, int *error)
+{
+    int errToSet = 0;
+
+    if (args == NULL)
+    {
+        errToSet = E_CLP_NULL;
+    }
+    else
+    {
+        for (Option *runner = args->internal_list; runner; runner = runner->next)
+        {
+            printf("option: %c --- param: %s\n", runner->op, runner->param);
+        }
+    }
+
+    error && (*error = errToSet);
+}
+
+Option *CommandLineParser_getArguments(Arguments args, int *error)
+{
+    int errToSet = 0;
+    Option* toRet = NULL;
+
+    if (args == NULL)
+    {
+        errToSet = E_CLP_NULL;
+    }
+    else
+    {
+        toRet = args->internal_list;
+    }
+
+    error && (*error = errToSet);
+    return toRet;
+}
+
+const char *logger_error_messages[] = {
+    "",
+    "command line parser internal malloc error",
+    "command line parser is NULL",
+    "command line parser internal general error",
+    "command line parser has received wrong input",
+};
+
+const char *CommandLineParser_getErrorMessage(int errorCode)
+{
+    return logger_error_messages[errorCode];
+}
---
+++ command-line-parser/command-line-parser.h
@@ -0,0 +1,26 @@
+#include <stddef.h>
+
+#ifndef COMMAND_LINE_PARSER__
+#define COMMAND_LINE_PARSER__
+
+#define E_CLP_MALLOC 1
+#define E_CLP_NULL 2
+#define E_CLP_ERROR 3
+#define E_CLP_WRONG_INPUT 4
+
+typedef struct Arguments *Arguments;
+
+typedef struct Option
+{
+    char op;
+    const char *param;
+    struct Option *next;
+} Option;
+
+const char *CommandLineParser_getErrorMessage(int errorCode);
+Arguments CommandLineParser_parseArguments(char *argv[], int *error);
+Option *CommandLineParser_getArguments(Arguments args, int *error);
+void CommandLineParser_printArguments(Arguments args, int *error);
+void CommandLineParser_delete(Arguments *argsPtr, int *error);
+
+#endif
